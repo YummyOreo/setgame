ef4a9efcaf88a819eb6f1cc16ccb88f939145165
Quinn <theyumoreos@gmail.com>
2025-08-15 16:11:15 -0500

    feat: finish

--- setgame.js
+++ setgame.js
@@ -11,9 +11,9 @@ const Shape = {
 }
 
 const Fill = {
-    OPEN: 0,
-    LINED: 1,
-    FILLED: 2
+    OPEN: "open",
+    LINED: "lined",
+    FILLED: "filled"
 }
 
 class Card {
@@ -56,7 +56,7 @@ function generateGreen(shape, fill) {
     return [
         new Card(shape, Color.GREEN, 1, fill),
         new Card(shape, Color.GREEN, 2, fill),
-        new Card(shape, Color.GREEn, 3, fill)
+        new Card(shape, Color.GREEN, 3, fill)
     ];
 }
 function generateBlue(shape, fill) {
@@ -92,11 +92,109 @@ function randomizeCards() {
     }
 }
 
-generateCards();
-randomizeCards();
+const setsElement = document.querySelector(".sets")
+const cardsLeft = document.querySelector(".left")
+
 
 let rows = document.querySelectorAll(".row");
 let index = 0;
+
+let activeList = {};
+
+let sets = 0;
+
+generateCards();
+randomizeCards();
+updateStats()
+
+function checkProperty(list) {
+    const left = list[0] == list[1];
+    const right = list[1] == list[2];
+
+    console.log(left + "" + right)
+
+    return (left && right) || (!left && !right);
+}
+function checkSet(list) {
+    if (!checkProperty([list[0].shape, list[1].shape, list[2].shape])) {
+    console.log("a")
+        return false;
+    }
+
+    if (!checkProperty([list[0].color, list[1].color, list[2].color])) {
+    console.log("b")
+        return false;
+    }
+
+    if (!checkProperty([list[0].number, list[1].number, list[2].number])) {
+    console.log("c")
+        return false;
+    }
+
+    if (!checkProperty([list[0].fill, list[1].fill, list[2].fill])) {
+    console.log("d")
+        return false;
+    }
+
+    return true;
+}
+
+function updateStats() {
+    setsElement.textContent = "Sets found: " + sets;
+    cardsLeft.textContent = "Cards remaining: " + (cards.length - index);
+}
+
+function removeCards(keys) {
+    for (let key in keys) {
+        key = keys[key];
+        let el = activeList[key];
+        el.classList.toggle("active");
+        const newEl = cards[index].display();
+        newEl.setAttribute("index", index);
+        newEl.addEventListener("click", handleClick)
+        el.replaceWith(newEl);
+        index++;
+        delete activeList[key];
+    }
+    sets++;
+    updateStats()
+}
+
+function winCheck() {
+    return index == cards.length;
+}
+
+function handleClick(e) {
+    const el = e.target;
+    const index = el.getAttribute("index");
+    el.classList.toggle("active")
+    if (activeList[index] == undefined) {
+        if (Object.keys(activeList).length >= 2) {
+            activeList[index] = el;
+            let list = Object.keys(activeList).flatMap((id) => {
+                return cards[id];
+            });
+            let keys = Object.keys(activeList);
+            if (checkSet(list)) {
+                if (winCheck()) {
+                    document.querySelector(".setgame").innerHTML = "<h2>You Win!</h2>";
+                }
+                removeCards(keys)
+            } else {
+                for (let key in keys) {
+                    key = keys[key];
+                    let el = activeList[key];
+                    el.classList.toggle("active");
+                    delete activeList[key];
+                }
+            }
+        } else {
+            activeList[index] = el;
+        }
+    } else {
+        delete activeList[index]
+    }
+}
 
 for (let i = 0; i < rows.length; i++) {
     const row = rows[i];
@@ -105,7 +203,8 @@ for (let i = 0; i < rows.length; i++) {
         const item = columns[b];
         const el = cards[index].display();
         el.setAttribute("index", index);
-        item.appendChild(cards[index].display());
+        el.addEventListener("click", handleClick)
+        item.append(el);
         index = index + 1;
     }
 }
